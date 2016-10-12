57d1d7f84c0b259ff0a79c5dad7b6040f8301401
risha1588 <rishsweet.risha@gmail.com>
2016-10-13 01:07:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,33 +6,94 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne = {
-    title: 'Article One | Risha Rahman',
-    heading: 'Article One',
-    date: '1 Oct 2016',
-    content: `  <p>
-                This is the content for my first article. This is the content for my first article.
-                This is the content for my first article. This is the content for my first article.
-                This is the content for my first article. This is the content for my first article.
-                This is the content for my first article. This is the content for my first article.
-               </p>
-               <p>
-                 This is the content for my first article.This is the content for my first article.This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-             </p> '
-}
 
+    
+    
+var articles = {
+articleOne = {
+            title: 'Article One | Risha Rahman',
+            heading: 'Article One',
+            date: '1 Oct 2016',
+            content: `  <p>
+                        This is the content for my first article. This is the content for my first article.
+                        This is the content for my first article. This is the content for my first article.
+                        This is the content for my first article. This is the content for my first article.
+                        This is the content for my first article. This is the content for my first article.
+                       </p>
+                       <p>
+                         This is the content for my first article.This is the content for my first article.This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                     </p> `
+},
+    articleTwo = {
+        title: 'Article Two | Risha Rahman',
+        heading: 'Article Two',
+        date: '3 Oct 2016',
+        content: `<html>
+    
+            <p>
+                This is the content for my second article.
+            </p>
+            
+        `;
+    },
+    articleThree = {
+        title: 'Article Two | Risha Rahman',
+        heading: 'Article Two',
+        date: '3 Oct 2016',
+        content: `
+            <p>
+                This is the content for my third article.
+            </p>
+        `;
+    },
+    
+    
+
+
+function createTemplate (data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+var htmlTemplate = `<html>
+    <head>
+        <title> 
+        ${title}
+        </title>
+        <meta name="viewport" content="width=device=width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+        <a href="/"> Home </a>
+        </div>
+        <hr/>
+        <h3>${heading}</h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+   return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.send(createTemplate(articleTwo));
 });
 app.get('/article-three',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+   res.send(createTemplate(articleThree)); 
 });
 
 app.get('/ui/style.css', function (req, res) {

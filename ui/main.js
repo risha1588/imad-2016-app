60ed465ee4535026ea350ceaec735f044499d45e
risha1588 <rishsweet.risha@gmail.com>
2016-11-05 17:40:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
-//Change the text of the main-text div
-var element=document.getElementById('main-text');
+// Change the text of the main-text div
+var element=document.getElementById('main-text'
+);
+
 element.innerHTML = 'New Value';
+
+var img = document.getElementById('img');
+img.onclick = function() {
+    img.tyle.marginLeft = '100px';
+};

4c8e69c456828a3ac32d19df86afd7acfeab37d4
risha1588 <rishsweet.risha@gmail.com>
2016-11-05 22:41:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,1 @@
 console.log('Loaded!');
-// Change the text of the main-text div
-var element=document.getElementById('main-text'
-);
-
-element.innerHTML = 'New Value';
-
-var img = document.getElementById('madi');
-
-var marginLeft = 0;
-function moveRight() {
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function() {
-   var interval=setInterval(moveRight,50);
-};

cea9f91fca942782c48fae041eda27429ac50116
risha1588 <rishsweet.risha@gmail.com>
2016-11-06 00:06:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,25 @@
 // Counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function() {
     
-    // Make a request to the counter end point
-    
+    // Create a request object
+    var request = new XMLHttpRequest();
     // Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readystate == XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status == 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not done yet 
+    };
+    // Make the request
+                request.open('GET', 'http://risha1588.imad.hasura-app.io/counter', true);
+                request.send(null);
     
-    // Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
 };
